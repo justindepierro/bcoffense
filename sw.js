2f3013fe6135dae21e5967d716208ef231c731ed
Justin DePierro <jdepierro@burkecatholic.org>
2026-02-23 18:33:44 -0500

    bump SW cache to v4 for tier-5 asset changes

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *   - Stale-while-revalidate: serve cached, then update cache in background
  */
 
-const CACHE_NAME = "bcoffense-v3";
+const CACHE_NAME = "bcoffense-v4";
 
 // Allow the app to trigger a cache refresh
 self.addEventListener("message", (event) => {
