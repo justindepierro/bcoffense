c11c7ad88ef2231254a40834e2856933cc05fa4a
Justin DePierro <jdepierro@burkecatholic.org>
2026-04-25 20:57:04 -0400

    style: shorten script play cards (SW v188)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *   - Stale-while-revalidate: serve cached, then update cache in background
  */
 
-const CACHE_NAME = "bcoffense-v187";
+const CACHE_NAME = "bcoffense-v188";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
