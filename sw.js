510719a4dd2a28c85010c1ba6e786413187c728e
Justin DePierro <jdepierro@burkecatholic.org>
2026-04-26 08:53:36 -0400

    fix: unclip sticky script rail (SW v198)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *   - Stale-while-revalidate: serve cached, then update cache in background
  */
 
-const CACHE_NAME = "bcoffense-v197";
+const CACHE_NAME = "bcoffense-v198";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
