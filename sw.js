424f974fb8676d923fc0a91101c53a7aca396235
Justin DePierro <jdepierro@burkecatholic.org>
2026-05-01 10:02:58 -0400

    refactor(wristband): extract export module (SW v260)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *   - Stale-while-revalidate: serve cached, then update cache in background
  */
 
-const CACHE_NAME = "bcoffense-v259";
+const CACHE_NAME = "bcoffense-v260";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
@@ -80,6 +80,7 @@ const LOCAL_ASSETS = [
   "./js/script-storage.js",
   "./js/wristband.js",
   "./js/wristband-cards.js",
+  "./js/wristband-export.js",
   "./js/wristband-cell-popup.js",
   "./js/wristband-cell-actions.js",
   "./js/wristband-sort.js",
