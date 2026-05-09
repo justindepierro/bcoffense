73e699fbbfc2581bbead9360ff95deeafe4fe2a2
Justin DePierro <jdepierro@burkecatholic.org>
2026-05-09 18:41:58 -0400

    fix(wristband): trim printed wristband cards to 5"x3" (SW v409)
    - .wristband-print .wristband-card width 5.5in -> 5in so cut cards fit standard 5x3 wristband windows

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *   - Stale-while-revalidate: serve cached, then update cache in background
  */
 
-const CACHE_NAME = "bcoffense-v408";
+const CACHE_NAME = "bcoffense-v409";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
