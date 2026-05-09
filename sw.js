b828209bdc720ba111c75d499e63b1fb21814a66
Justin DePierro <jdepierro@burkecatholic.org>
2026-05-09 18:42:35 -0400

    fix(wristband): printed card height 3in -> 2.7in (SW v410)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *   - Stale-while-revalidate: serve cached, then update cache in background
  */
 
-const CACHE_NAME = "bcoffense-v409";
+const CACHE_NAME = "bcoffense-v410";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
