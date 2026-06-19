820abc0953588ea500a7dfa911e38b74f67cb16a
Justin DePierro <jdepierro@burkecatholic.org>
2026-06-19 09:14:57 -0400

    feat: extract installation render functions into separate file (SW v581)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *   - Stale-while-revalidate for other same-origin assets
  */
 
-const CACHE_NAME = "bcoffense-v580";
+const CACHE_NAME = "bcoffense-v581";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
@@ -124,6 +124,7 @@ const LOCAL_ASSETS = [
   "./js/script-vision.js",
   "./js/tendencies-render.js",
   "./js/tendencies.js",
+  "./js/installation-render.js",
   "./js/installation.js",
   "./js/identity.js",
   "./js/offensebuilder.js",
