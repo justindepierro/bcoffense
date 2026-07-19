55e37fe0937f3867944c2240d727167b1cac3aa6
Justin DePierro <justindepierro@Justins-MacBook-Pro.local>
2026-07-19 18:37:07 -0400

    Stabilize script print table background

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *   - Stale-while-revalidate for other same-origin assets
  */
 
-const CACHE_NAME = "bcoffense-v1257";
+const CACHE_NAME = "bcoffense-v1258";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
