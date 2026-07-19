f9ed811874b472138b53e3f457d276fc39e7b0b8
Justin DePierro <justindepierro@Justins-MacBook-Pro.local>
2026-07-19 13:51:09 -0400

    Anchor sticky script worksheet headers

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *   - Stale-while-revalidate for other same-origin assets
  */
 
-const CACHE_NAME = "bcoffense-v1241";
+const CACHE_NAME = "bcoffense-v1242";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
