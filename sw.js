1e3d25ff40b19dae0d5ed9f955f2c31eef202e5a
Justin DePierro <justindepierro@Justins-MacBook-Pro.local>
2026-08-06 09:30:24 -0400

    Tighten Index Card preview and print density

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *   - Stale-while-revalidate for other same-origin assets
  */
 
-const CACHE_NAME = "bcoffense-v1613";
+const CACHE_NAME = "bcoffense-v1614";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
