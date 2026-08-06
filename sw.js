67e61210f8115253ee2ba4e5972709819542cf1b
Justin DePierro <justindepierro@Justins-MacBook-Pro.local>
2026-08-06 09:20:03 -0400

    Compact Index Card editor controls

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *   - Stale-while-revalidate for other same-origin assets
  */
 
-const CACHE_NAME = "bcoffense-v1609";
+const CACHE_NAME = "bcoffense-v1610";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
