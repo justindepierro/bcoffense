562c5f52c55366dd4ccd9d2c82a3f1035eeb56fd
Justin DePierro <justindepierro@Justins-MacBook-Pro.local>
2026-08-06 22:02:48 -0400

    Bump app shell cache to v1634

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *   - Stale-while-revalidate for other same-origin assets
  */
 
-const CACHE_NAME = "bcoffense-v1633";
+const CACHE_NAME = "bcoffense-v1634";
 
 const NETWORK_FIRST_PATTERNS = [
   /\/index\.html$/,
